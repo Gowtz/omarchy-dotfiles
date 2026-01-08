3c62d81768b81657323e73b194feb83cdbed604c
Gowtham V <gajavijay8@gmail.com>
2026-01-08 12:38:00 +0530

    temp save

--- fish/config.fish
+++ fish/config.fish
@@ -2,48 +2,37 @@ function fish_greeting
 end
 function fish_right_prompt
 end
-function config
-  git --git-dir=$HOME/.config/dotfiles.git --work-tree=$HOME $argv
-end
+
+set -x OMARCHY_SCREENSHOT_DIR "$HOME/Pictures/Screenshot"
 
 alias py "source ~/.dev/env/bin/activate.fish"
-alias xampp "sudo /opt/lampp/manager-linux-x64.run"
-alias ll "eza -lF --icons"
-alias llt "eza --icons --tree -L 2"
-alias mysql "sudo /opt/lampp/bin/mysql"
+alias ll "eza -l --icons"
+
 # alias tmux "tmux -u"
-alias bsp "bspc config top_padding 0"
-alias nll "nmcli device wifi list"
-alias nmc "nmcli device wifi connect"
-# alias ollama "docker exec -it ollama ollama run llama3.2"
-alias sql "docker exec -it DB psql -U gowtz"
-alias changebg "feh --bg-fil --randomize ~/Pictures/wallpaper/random/*"
+
+alias sql "docker exec -it db psql -u gowtz"
+
 alias neo fastfetch
-alias psss "ansible-vault view ~/.dev/Pass/PASS.csv"
+alias psss "ansible-vault view ~/.dev/pass/PASS.csv"
 alias please sudo
 alias f-conf "nvim ~/.config/fish/config.fish"
-alias pn pnpm
-alias vim nvim
-alias bat batcat
 alias disp " ddcutil setvcp 10 "
 alias btc "bluetoothctl connect"
-alias pc "pactl set-default-sink"
+alias lz "lazygit"
 
-# set -gx PNPM_HOME "/home/gowtham/.local/share/pnpm"
 
-# if not string match -q -- $PNPM_HOME $PATH
-#     set -gx PATH "$PNPM_HOME" $PATH
+# set -gx pnpm_home "/home/gowtham/.local/share/pnpm"
+# if not string match -q -- $pnpm_home $path
+#     set -gx path "$pnpm_home" $path
 # end
-#
-set -Ux PATH $PATH $HOME/.flutter/bin
-set -Ux PATH $PATH $HOME/.flutter/bin
+# set -ux path $path $home/.flutter/bin
 
 # opencode
-fish_add_path /home/gowtz/.opencode/bin
+# fish_add_path /home/gowtz/.opencode/bin
 
 # pnpm
-set -gx PNPM_HOME "/home/gowtz/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
+# set -gx pnpm_home "/home/gowtz/.local/share/pnpm"
+# if not string match -q -- $pnpm_home $path
+#   set -gx path "$pnpm_home" $path
+# end
 # pnpm end
